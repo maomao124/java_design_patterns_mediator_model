2e2e611abecabb6b10abdf068bc3829ab19d01d1
mao <1296193245@qq.com>
2022-08-21 15:38:55 +0800

    java_design_patterns_mediator_model

--- src/main/java/mao/t1/Test.java
+++ src/main/java/mao/t1/Test.java
@@ -17,6 +17,40 @@ public class Test
 {
     public static void main(String[] args)
     {
+        MediatorStructure mediator = new MediatorStructure();
+
+        HouseOwner houseOwner1 = new HouseOwner("张三", mediator);
+        HouseOwner houseOwner2 = new HouseOwner("李四", mediator);
+        HouseOwner houseOwner3 = new HouseOwner("王五", mediator);
+
+        Tenant tenant1 = new Tenant("赵六", mediator);
+        Tenant tenant2 = new Tenant("张九", mediator);
+        Tenant tenant3 = new Tenant("王四", mediator);
+        Tenant tenant4 = new Tenant("钱七", mediator);
+
+        //添加
+        mediator.addHouseOwners(houseOwner1);
+        mediator.addHouseOwners(houseOwner2);
+        mediator.addHouseOwners(houseOwner3);
+
+        mediator.addTenants(tenant1);
+        mediator.addTenants(tenant2);
+        mediator.addTenants(tenant3);
+        mediator.addTenants(tenant4);
+
+        tenant2.constact("我需要一套30平的房子,联系人：" + tenant2.getName());
+        System.out.println("----");
+        tenant4.constact("我需要一套70平的房子,联系人：" + tenant4.getName());
+        System.out.println("----");
+        tenant1.constact("我需要一套100平的房子,价格3000一个月以上的,联系人：" + tenant1.getName());
+
+        System.out.println("----");
+        System.out.println("----");
+
+        houseOwner1.constact("现有一套120平的房子出租，价格为4000元一个月,出租人：" + houseOwner1.getName());
+        System.out.println("----");
+        houseOwner3.constact("现有一套40平的房子出租，价格为1300元一个月,出租人：" + houseOwner3.getName());
+
 
     }
 }
